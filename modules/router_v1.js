078f33b4c140996b46ab66cfbaa61f7a7f28bed5
basiji <40441882+basiji@users.noreply.github.com>
2018-09-27 10:25:13 +0330

    x

--- modules/router_v1.js
+++ modules/router_v1.js
@@ -1,11 +1,16 @@
+// Express
 var express = require('express');
 var path = require('path');
+
+// Constants
+var constants = require('./constants');
 
 // Define functions
 var register = require('./v1/register');
 var update = require('./v1/update');
 var fuckoff = require('./v1/fuckoff.js');
 var submitPayment = require('./v1/payment.js');
+
 
 module.exports = function(connection){
     
@@ -39,6 +44,16 @@ module.exports = function(connection){
     // Decode Bank Information
     router.get('/fuckoff',function(req, res){
         fuckoff(req, res, connection);
+    });
+
+    // Ping
+    router.get('/ping', function(req, res){
+
+        if(req.query.package === constants.PACKAGE_NAME)
+            return res.sendStatus(200);
+        else 
+            return res.sendStatus(404);
+
     });
 
     return router;
